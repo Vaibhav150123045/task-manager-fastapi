a09aa60d688ca06cc942b8f9b0fc55bce1818c3c
Vaibhav Motwani <motwani777@gmail.com>
2025-09-04 14:30:01 +0200

    added create and update item functionalities

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    id: int
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
+
+items = {}
 
 
 @app.get("/")
@@ -11,4 +23,22 @@ def read_root() -> dict[str, Union[str, int]]:
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    if item_id not in items:
+        return {"error": "Item not found"}
+    # item_details =
+    return {"item_id": item_id, "item": items[item_id]}
+
+
+@app.post("/items/")
+def create_item(item: Item) -> dict[str, Union[str, float, None]]:
+    item_dict = item.model_dump()
+    item_id = item.id
+    items[item_id] = item
+    return item_dict
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item) -> dict[str, Union[int, str, float, None]]:
+    # Update the item in the in-memory storage
+    items[item_id] = item
+    return {"item_id": item_id, **item.model_dump()}
