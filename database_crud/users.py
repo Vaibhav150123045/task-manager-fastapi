152d3f195bce197d305c6c236556613f9e6ebb34
Vaibhav Motwani <motwani777@gmail.com>
2025-09-10 20:05:57 +0200

    added database queries for creating new user

---
+++ database_crud/users.py
@@ -0,0 +1,33 @@
+from sqlalchemy.orm import Session
+from models import User, UserCredential
+from schemas import UserCreate
+from datetime import datetime
+from passlib.hash import bcrypt
+
+
+def create_new_user(db: Session, user: UserCreate):
+    # Create user profile
+    new_user = User(
+        username=user.username,
+        name=user.name,
+        DOB=user.DOB,
+        email=user.email,
+        created_at=datetime.now(),
+        updated_at=datetime.now()
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    # Create user credentials
+    hashed_pw = bcrypt.hash(user.password)
+    new_cred = UserCredential(
+        userID=new_user.userID,
+        hashed_password=hashed_pw,
+        isCurrent=True
+    )
+    db.add(new_cred)
+    db.commit()
+    db.refresh(new_cred)
+
+    return new_user
