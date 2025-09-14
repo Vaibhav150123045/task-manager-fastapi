0fbc9d639bb8e2fea9c55566759b48165abae28c
Vaibhav Motwani <motwani777@gmail.com>
2025-09-14 23:44:05 +0200

    added sql schemas for tasks

---
+++ schemas/tasks.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+
+
+class TaskBase(BaseModel):
+    title: str
+    description: str
+    completed: bool = False
+
+
+class TaskCreate(TaskBase):
+    pass
+
+
+class TaskUpdate(TaskBase):
+    pass
+
+
+class TaskOut(TaskBase):
+    id: int
+
+    class Config:
+        orm_mode = True
